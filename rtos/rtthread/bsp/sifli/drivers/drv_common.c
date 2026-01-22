dfa897f0eccef17010fd799c92bae0a183b4c5c5
Gerard Marull-Paretas <gerard@teslabs.com>
2026-01-22 09:37:39 +0800

    [chore][rtos][rtthread] Use HAL_RCC_HCPU_SetTickDiv
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- rtos/rtthread/bsp/sifli/drivers/drv_common.c
+++ rtos/rtthread/bsp/sifli/drivers/drv_common.c
@@ -131,8 +131,7 @@ __ROM_USED void rt_hw_systick_init(void)
     HAL_RCC_HCPU_ClockSelect(RCC_CLK_MOD_HP_TICK, RCC_CLK_TICK_HRC48);
     /* workaround: add delay to avoid systick config failure in some chips due to known reason */
     HAL_Delay_us(200);
-    MODIFY_REG(hwp_hpsys_rcc->CFGR, HPSYS_RCC_CFGR_TICKDIV_Msk,
-               MAKE_REG_VAL(60, HPSYS_RCC_CFGR_TICKDIV_Msk, HPSYS_RCC_CFGR_TICKDIV_Pos));
+    HAL_RCC_HCPU_SetTickDiv(60);
     HAL_SYSTICK_Config(800000 / RT_TICK_PER_SECOND);
     HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK_DIV8);
 
