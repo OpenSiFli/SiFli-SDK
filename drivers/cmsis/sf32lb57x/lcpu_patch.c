2910fda78d2ed6b3ff6382d92722a24f12539dde
zichuancui <zichuancui@sifli.com>
2026-07-29 19:35:11 +0800

    [bug][bt] update RF driver and lcpu patch
    redmine: #12337, REDMINE-id
    
    ext-redmine: #id, REDMINE-id
    
    [Description in detail]
    1、fix 56x MAC error(hop under run error)，lcpu patch version：0x74,  bug12337
    2、fix 57x no-signaling power issue
    3、update 57 RF driver
    4、fix 57x ble central update connection para issue. lcpu patch version:2
    5、fix 58x ble update connection para assert issue.lcpu patch version:9   bug12317
    
    Change-Id: I102dbfeb182ea256ec3bc2e782a0cbc57f9ce3b2

--- drivers/cmsis/sf32lb57x/lcpu_patch.c
+++ drivers/cmsis/sf32lb57x/lcpu_patch.c
@@ -11,10 +11,15 @@
 #include "register.h"
 #include "bf0_hal_patch.h"
 #ifdef HAL_LCPU_PATCH_MODULE
-const unsigned int g_lcpu_patch_list[] = {   0x50544348, 0x00000000,};
-const unsigned int g_lcpu_patch_bin[] = {    0x49064805, 0x48066001, 0x70012101, 0xF44F4805,
-                                             0xF8C02100, 0x477010C4, 0x204001F4, 0x00410035,
-                                             0x20400A4C, 0x20400954, 0xBF082831, 0x2F00F5B1,
+const unsigned int g_lcpu_patch_list[] = {   0x50544348, 0x00000010, 0x00031178, 0xBF5CF3DE,
+                                             0x0002FEE4, 0xB8B6F3E0,
+                                         };
+const unsigned int g_lcpu_patch_bin[] = {    0x49064805, 0x48066001, 0x70012102, 0xF44F4805,
+                                             0xF8C02100, 0x477010C4, 0x204001F4, 0x00410065,
+                                             0x20400A4C, 0x20400954, 0x0005F895, 0xF895B938,
+                                             0xB920002D, 0x107FF241, 0x0003F2C0, 0xF2414700,
+                                             0xF2C0108B, 0x47000003, 0x162CF240, 0xF64F1B92,
+                                             0xF2C066ED, 0x47300602, 0xBF082831, 0x2F00F5B1,
                                              0x480AD112, 0x4A0C490A, 0x1400F8C0, 0x600A4909,
                                              0x604A4A0A, 0x6801220D, 0x210EF362, 0x60012204,
                                              0xF3626801, 0x60010106, 0x47702000, 0x40090490,
@@ -22,7 +27,7 @@ const unsigned int g_lcpu_patch_bin[] = {    0x49064805, 0x48066001, 0x70012101,
                                         };
 void lcpu_patch_install()
 {
-    uint32_t entry[3] = {0x48434150, 0x1, LCPU_PATCH_BUF_START_ADDR + 13};
+    uint32_t entry[3] = {0x48434150, 0x2, LCPU_PATCH_BUF_START_ADDR + 13};
     memcpy((void *)LCPU_PATCH_BUF_START_ADDR, (void *)&entry, 12);
 #ifdef SOC_BF0_HCPU
     memset((void *)(LCPU_PATCH_BUF_START_ADDR + 12), 0, LCPU_PATCH_TOTAL_SIZE);
