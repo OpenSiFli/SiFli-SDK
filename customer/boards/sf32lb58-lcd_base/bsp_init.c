6b6b4a6839bfa5a8ebd4b181828f1c155b8b7e42
Xiaoming Xi <xiaomingxi@sifli.com>
2026-09-22 14:48:23 +0800

    [bug][board] set hdiv to make default value 1 take effect after wakeup from standby
    ext-redmine: #5199, REDMINE-5119
    
    After standby wake-up, HDIV has a default value of 1, but its actual effect is equivalent to 2.
    If HDIV is not explicitly set to 1, HCLK runs at 120 MHz during PSRAM initialization, which may cause PSRAM access failures.
    
    Change-Id: Ib3f6b682d584eb1195b2ce21c070927fcd856aee

--- customer/boards/sf32lb58-lcd_base/bsp_init.c
+++ customer/boards/sf32lb58-lcd_base/bsp_init.c
@@ -113,6 +113,8 @@ void HAL_PreInit(void)
 
     HAL_RCC_HCPU_EnableDLL1(240000000);
     HAL_RCC_HCPU_ClockSelect(RCC_CLK_MOD_SYS, RCC_SYSCLK_DLL1);
+    /* set hdiv to make default value 1 take effect after wakeup from standby */
+    HAL_RCC_HCPU_SetDiv(1, 2, 5);
 
     // Reset sysclk used by HAL_Delay_us
     HAL_Delay_us(0);
@@ -154,8 +156,6 @@ void HAL_PreInit(void)
 #endif
     }
 #endif
-
-    HAL_RCC_HCPU_SetDiv(1, 2, 5);
 
     HAL_RCC_HCPU_DeepWFIClockSelect(true, RCC_SYSCLK_HXT48);
     HAL_RCC_HCPU_SetDeepWFIDiv(48, 0, 1);
